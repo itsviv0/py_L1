c2bf6b86a2092147ba52fe8bba19264e6edd780b
Vivek Sindagi <93273111+itsviv0@users.noreply.github.com>
2022-08-12 19:02:26 +0530

    first commit

---
+++ 0_print_your_name.py
@@ -0,0 +1,9 @@
+def input_name():
+    name = input("Enter your name: ")
+    return name
+    
+def output_name(name):
+    print( "Your name is " + name)
+    
+your_name = input_name()
+output_name(your_name)
